8327bfbf16b77976cd99f97ce7891d77a3510289
John <>
2020-10-26 21:23:00 -0400

    - reinitialized the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'; 
-//import { HttpModule } from '@angular/common/http'; 
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-//    HttpModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
